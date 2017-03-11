25176c12acd5cd75cc15d924c5af71893ba968fb
rileylittlefield <rlittlefield@manifestcorp.com>
2017-03-11 00:54:38 -0500

    adds basic grid behavior

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('scss', ['compile-sass']);
-gulp.task('wscss', ['watch-compile-sass']);
+gulp.task('scss', ['compile-scss']);
+gulp.task('wscss', ['watch-compile-scss']);
 
 gulp.task('compile-scss', function () {
-  return gulp.src('./styles/site.scss')
+  return gulp.src('./styles/example/newspaper-stylesheet.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./src/main/webapp'));
+    .pipe(gulp.dest('./styles/target'));
 });
 
 gulp.task('watch-compile-scss',['compile-scss'], function () {
